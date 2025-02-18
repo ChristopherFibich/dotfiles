b0cfa1acfe2d2b6febfb8264ce9ce20a03fcae30
Christopher <fibich.christopher@gmx.de>
2025-02-18 18:33:08 +0100

    added fallback screen layout fixed dp-2 scrren size

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --off --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-3 --mode 1920x1080 --pos 0x1080 --rotate normal
+xrandr --output DVI-D-1 --off --output DP-1 --off --output DP-2  --set "underscan hborder" 51 --set "underscan vborder" 30 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-3 --mode 1920x1080 --pos 0x1080 --rotate normal
+
+
